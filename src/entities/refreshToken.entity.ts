9e402687876032b62cf9433cc2ec1b083e0cd188
Wasiu Bakare <wasiubakare@gmail.com>
2025-05-03 16:57:28 +0100

    feat: add fields needed for onboarding and class validator decorators

--- src/entities/refreshToken.entity.ts
+++ src/entities/refreshToken.entity.ts
@@ -1,3 +1,10 @@
+import { Exclude } from "class-transformer";
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from "class-validator";
 import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import ExtendedBaseEntity from "./base.entity";
 import { UserEntity } from "./user.entity";
@@ -5,14 +12,19 @@ import { UserEntity } from "./user.entity";
 @Entity({ name: "refresh_tokens" })
 export class RefreshToken extends ExtendedBaseEntity {
   @Column({ nullable: false })
+  @IsString()
+  @IsNotEmpty()
   token: string;
 
   @Column({ name: "is_valid", type: "boolean", default: true })
+  @IsBoolean()
   is_valid: boolean;
 
+  @Exclude()
   @ManyToOne(() => UserEntity, (user) => user.refresh_tokens, {
     onDelete: "CASCADE",
   })
   @JoinColumn({ name: "user_id" })
+  @ValidateNested()
   user: UserEntity;
 }
