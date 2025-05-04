f7e351ebc9068094a51c4190185ddc89ca20a4fe
Wasiu Bakare <wasiubakare@gmail.com>
2025-05-04 13:11:19 +0100

    refactor(middleware): make upload middleware reusable across different file types

--- src/middlewares/uploadResume.ts
+++ src/middlewares/uploadResume.ts
@@ -1,41 +1,7 @@
-import { AppError } from "@src/exceptions/appError";
-import { Request } from "express";
-import multer from "multer";
-import path from "path";
+import { createFileUploader } from "@src/utils/createFileUploader";
 
-const storage = multer.diskStorage({
-  destination: (
-    _req: Request,
-    _file: Express.Multer.File,
-    cb: (error: Error | null, destination: string) => void,
-  ) => {
-    cb(null, "uploads/resumes/");
-  },
-  filename: (
-    _req: Request,
-    file: Express.Multer.File,
-    cb: (error: Error | null, filename: string) => void,
-  ) => {
-    const ext = path.extname(file.originalname);
-    const uniqueName = `${Date.now()}-${file.fieldname}${ext}`;
-    cb(null, uniqueName);
-  },
+export const uploadResume = createFileUploader({
+  destinationFolder: "uploads/resumes",
+  fieldname: "resume",
+  allowedMimeTypes: ["application/pdf"],
 });
-
-const fileFilter = (
-  req: Request,
-  file: Express.Multer.File,
-  cb: (error: Error | null, isUploaded: boolean) => void,
-) => {
-  if (file.mimetype === "application/pdf") {
-    cb(null, true);
-  } else {
-    cb(new AppError("Only PDF files are allowed", 400), false);
-  }
-};
-
-export const uploadResume = multer({
-  storage,
-  limits: { fileSize: 2 * 1024 * 1024 },
-  fileFilter,
-}).single("resume");
