635781843ccbaea54dc5c77f950841ba21653763
Wasiu Bakare <wasiubakare@gmail.com>
2025-05-14 21:04:04 +0100

    refactor: extend UserEntity and /me to include dashboard summary

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,12 @@
+import { DashboardService } from "@src/dashboard/services/dashboard.service";
 import AppDataSource from "@src/datasource";
-import { UserEntity } from "@src/entities/user.entity";
+import { UserEntity, UserRole } from "@src/entities/user.entity";
 import { NotFoundError } from "@src/exceptions/notFoundError";
 import { sanitizeUser } from "@src/utils/sanitizeUser";
 
 export class UserService {
   private userRepo = AppDataSource.getRepository(UserEntity);
+  private dashboardService = new DashboardService();
 
   async getCurrentUser(userId: string) {
     const user = await this.userRepo.findOne({
@@ -12,6 +14,17 @@ export class UserService {
       relations: ["talent_profile", "recruiter_profile"],
     });
     if (!user) throw new NotFoundError("User not found");
-    return sanitizeUser(user);
+
+    const sanitized = sanitizeUser(user);
+
+    let dashboardData = null;
+    if (user.role === UserRole.TALENT) {
+      dashboardData = await this.dashboardService.getTalentDashboard(userId);
+    }
+
+    return {
+      ...sanitized,
+      dashboard: dashboardData,
+    };
   }
 }
