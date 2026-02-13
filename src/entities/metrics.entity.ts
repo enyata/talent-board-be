0b95150e1888367b9a56dcd590dd7c1794278a7a
Wasiu Bakare <wasiubakare@gmail.com>
2026-02-13 17:56:25 +0100

    fix: add validation decorators and default values

--- src/entities/metrics.entity.ts
+++ src/entities/metrics.entity.ts
@@ -1,3 +1,4 @@
+import { IsInt, Min } from "class-validator";
 import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
 import ExtendedBaseEntity from "./base.entity";
 import { UserEntity } from "./user.entity";
@@ -9,14 +10,22 @@ export class MetricsEntity extends ExtendedBaseEntity {
   user: UserEntity;
 
   @Column({ default: 0 })
-  upvotes: number;
+  @IsInt()
+  @Min(0)
+  upvotes: number = 0;
 
   @Column({ default: 0 })
-  profile_views: number;
+  @IsInt()
+  @Min(0)
+  profile_views: number = 0;
 
   @Column({ default: 0 })
-  recruiter_saves: number;
+  @IsInt()
+  @Min(0)
+  recruiter_saves: number = 0;
 
   @Column({ default: 0 })
-  weekly_search_appearances: number;
+  @IsInt()
+  @Min(0)
+  weekly_search_appearances: number = 0;
 }
